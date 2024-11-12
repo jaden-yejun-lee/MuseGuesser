c333f626f7f6ed16944569f6d71839ca5b735c27
Haotian Yi <106487666+AnoldGH@users.noreply.github.com>
2024-11-12 15:38:39 -0800

    Revert "SpotifyProxy for requests, caching, and monitoring rate limit"

--- backend/routes/spotifyRouter.js
+++ backend/routes/spotifyRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const { getAccessToken } = require("../utility/tokenManager");
-const { SpotifyProxy } = require('../controller/spotifyProxy')
 
 // Get spotify access token with client credentials flow
 //  see https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
@@ -25,10 +24,18 @@ router.get('/token', async (req, res) => {
 // Get track from track ID
 router.get("/track/:id", async (req, res) => {
   const trackId = req.params.id;
-  const spotify = SpotifyProxy.getInstance();
 
   try {
-    res.json(spotify.getTrack(trackId))
+    const accessToken = await getAccessToken();
+    const response = await axios.get(
+      `https://api.spotify.com/v1/tracks/${trackId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch track data" });
   }
@@ -37,12 +44,19 @@ router.get("/track/:id", async (req, res) => {
 // Get recommendation from genre
 router.get("/recommendations", async (req, res) => {
   const { genres, limit = 30 } = req.query; // Default to "pop" if genres is not provided
-  const spotify = SpotifyProxy.getInstance();
+  const accessToken = await getAccessToken();
 
   try {
-    console.log("Requesting a random track of genre", genres)
-    const track = await spotify.getRandomTrackByGenre(genres)  // TODO: for now we assume there is one genre
-    res.json(track)
+    const response = await axios.get(
+      `https://api.spotify.com/v1/recommendations?seed_genres=${genres}&limit=${limit}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    res.json(response.data);
+
   } catch (error) {
     console.error("Error fetching recommendations:", error);
     res.status(500).json({ error: "Failed to fetch recommendations" });
