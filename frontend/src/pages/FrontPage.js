2d17239879d12a34cdcb97164f45c9edf640f412
Hakob3215 <hatajyan@gmail.com>
2024-11-22 17:49:11 -0800

    Add conditional rendering to FrontPage.js
    Add checks for signed in + completed daily challenge to FrontPage.js
    Change SignIn redirect to FrontPage (instead of dailyChallenge) if signed in

--- frontend/src/pages/FrontPage.js
+++ frontend/src/pages/FrontPage.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './styles/FrontPage.css';
 
 const FrontPage = () => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [doneDaily, setDoneDaily] = useState(false);
+    useEffect(() => {
+        const userData = localStorage.getItem("userData");
+        if (userData) {
+            const { userId, dailyScore } = JSON.parse(userData);
+            if (userId) {
+                setLoggedIn(true);
+            }
+            if (dailyScore !== -1) {
+                setDoneDaily(true);
+            }
+
+        } else {
+            setLoggedIn(false);
+        }
+    }, []);
+
+
     return (
         <div className="front-page">
             <h1>MuseGuesser</h1>
             <div className="links">
-                <Link to="/dailychallenge" className="link-button">Daily Challenge</Link>
-                <Link to="/signin" className="link-button">Sign In</Link>
-                <Link to="/signup" className="link-button">Sign Up</Link>
+                {!doneDaily && loggedIn ? <Link to="/dailychallenge" className="link-button">Daily Challenge</Link> : null}
+                {!loggedIn ?<>  
+                    <Link to="/signin" className="link-button">Sign In</Link>
+                    <Link to="/signup" className="link-button">Sign Up</Link>
+                </>: <Link to="/game" className="link-button">Play Game</Link>}
             </div>
         </div>
     );
