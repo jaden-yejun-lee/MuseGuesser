4f22527336e0d1b7b32e980f97d4d14bbb47b437
Kunal Patil <kunalpatil@Kunals-Air.fios-router.home>
2024-12-01 16:10:26 -0500

    Update styling

--- frontend/src/pages/CreateJoinRoom.js
+++ frontend/src/pages/CreateJoinRoom.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import './styles/CreateJoinRoom.css';
+
 
 const SERVER = process.env.REACT_APP_SERVER;
 
